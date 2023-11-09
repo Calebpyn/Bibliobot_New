7676e66fb3c096c56b8daf36ba26534c5ff805d6
Clanie1 <danybarociov@gmail.com>
2023-11-08 21:27:37 -0800

    prompt builded on the server

--- back/server.js
+++ back/server.js
@@ -1,63 +1,52 @@
-const express = require ('express');
-const cors = require('cors');
-const morgan = require('morgan')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const CONTEXT = "Dame una respuesta de un parrafo de solo dos lineas";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const { OpenAI } = require('openai');
+const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-    apiKey: process.env.TEST_KEY,
-})
+  apiKey: process.env.TEST_KEY,
+});
 
 const app = express();
 
 const port = 4000;
 
-const Routes = require('./routes/back.routes');
+const Routes = require("./routes/back.routes");
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(Routes);
 
-
-
 app.use((err, req, res, next) => {
+  console.log(err);
 
-    console.log(err)
-
-    res.status(404).json({
-        message: err
-    })
-
+  res.status(404).json({
+    message: err,
+  });
 });
 
 //Cahtgpt test
 
 app.post("/chat", async (req, res) => {
+  const { text } = req.body;
+  const prompt = `${CONTEXT} Prompt: ${text} \n Fin del prompt.`;
+  const completition = await openai.chat.completions.create({
+    messages: [{ role: "system", content: prompt }],
+    model: "gpt-3.5-turbo",
+  });
 
-    const {prompt} = req.body;
-
-    const completition = await openai.chat.completions.create({
-        messages: [{ role: "system", content: prompt }],
-        model: "gpt-3.5-turbo",
-    });
-
-    console.log(completition.choices[0].message.content);
-
-    res.json(completition.choices[0].message);
-
-
-
-})
+  res.json(completition.choices[0].message);
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello")
-})
+  res.send("Hello");
+});
 
 app.listen(port, () => console.info(`Listening to port: ${port}`));
-
-
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,89 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import cetysMacot from '../imgs/cetysMascot.png'
-import {FaMicrophone} from 'react-icons/fa'
-import useSpeechRecognition from '../hooks/useSpeechRecognitionHook'
+import React, { useEffect, useState } from "react";
+import cetysMacot from "../imgs/cetysMascot.png";
+import { FaMicrophone } from "react-icons/fa";
+import useSpeechRecognition from "../hooks/useSpeechRecognitionHook";
 
 function Home() {
+  const actualUrl = "http://localhost:4000";
 
-    const actualUrl = 'http://localhost:4000'
+  const {
+    text,
+    isListening,
+    startListening,
+    stopListening,
+    hasRecognitionSupport,
+  } = useSpeechRecognition();
 
-    const {
-        text,
-        isListening,
-        startListening,
-        stopListening,
-        hasRecognitionSupport
-    } = useSpeechRecognition();
+  const [textArea, setTextArea] = useState(false);
 
-    const [textArea, setTextArea] = useState(false)
+  const [textVal, setTextVal] = useState("");
 
-    const [textVal, setTextVal] = useState("")
-
-    useEffect(() => {
-        setTextVal(text)
-        if(text != ""){
-            handleOpenAi()
-        }
-    }, [text])
-
-    const handleOpenAi = async () => {
-
-        console.log("fetching open ai endpoint...")
-        
-        try {
-
-            const bodyText = {
-                "prompt": `Te daré el siguiente prompt que proviene de una interfaz de usuario que integra el reconocimiento de voz. Por favor limita tu respuesta a máximo un párrafo de 2 líneas.
-                Prompt:
-                ${text}
-                Fin del prompt.
-                `
-            }
-
-            const response = await fetch(`${actualUrl}/chat`, {
-                method: "POST",
-                body: JSON.stringify(bodyText),
-                headers: { 'Content-Type': 'application/json' }
-            });
-
-            const data = await response.json();
-
-            setTextVal(data.content)
-            console.log(data.content)
-            
-
-        } catch (err) {
-            console.log(err)
-        }
-
+  useEffect(() => {
+    setTextVal(text);
+    if (text != "") {
+      handleOpenAi();
     }
+  }, [text]);
+
+  const handleOpenAi = async () => {
+    console.log("fetching open ai endpoint...");
+
+    try {
+      const bodyText = {
+        text: text,
+      };
+
+      const response = await fetch(`${actualUrl}/chat`, {
+        method: "POST",
+        body: JSON.stringify(bodyText),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await response.json();
+
+      setTextVal(data.content);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className='h-screen bg-yellow-500 w-full'>
-
-        
-        <div className='h-[65%] flex justify-center items-center'>
-
-            <div className='h-2/3 flex justify-center items-center'>
-                <div onClick={() => {
-                    stopListening()
-                }} className={!isListening ? 'absolute w-0 h-0 smooth flex justify-center items-center' : 'smooth rounded-full h-[17rem] w-[17rem] flex justify-center items-center absolute bg-green-700 shadow-2xl hover:cursor-pointer hover:scale-110'}>
-                    <FaMicrophone className={!isListening ? 'text-[0] smooth' : 'text-9xl smooth text-white'}/>
-                </div>
-                <img src={cetysMacot} onClick={() => {
-                    startListening();
-                    setTextArea(true)
-                }} className={!isListening ? 'h-full smooth hover:scale-110 hover:cursor-pointer' : 'h-0 smooth'}/>
-            </div>
-
+    <div className="h-screen bg-yellow-500 w-full">
+      <div className="h-[65%] flex justify-center items-center">
+        <div className="h-2/3 flex justify-center items-center">
+          <div
+            onClick={() => {
+              stopListening();
+            }}
+            className={
+              !isListening
+                ? "absolute w-0 h-0 smooth flex justify-center items-center"
+                : "smooth rounded-full h-[17rem] w-[17rem] flex justify-center items-center absolute bg-green-700 shadow-2xl hover:cursor-pointer hover:scale-110"
+            }
+          >
+            <FaMicrophone
+              className={
+                !isListening ? "text-[0] smooth" : "text-9xl smooth text-white"
+              }
+            />
+          </div>
+          <img
+            src={cetysMacot}
+            onClick={() => {
+              startListening();
+              setTextArea(true);
+            }}
+            className={
+              !isListening
+                ? "h-full smooth hover:scale-110 hover:cursor-pointer"
+                : "h-0 smooth"
+            }
+          />
         </div>
+      </div>
 
-        <div className='h-[35%] w-full flex justify-center items-center'>
-            <textarea className={!textArea ? 'w-0 smooth py-6' : 'w-2/3 py-6 px-10 rounded-full smooth shadow-2xl'} readOnly value={textVal}></textarea>
-        </div>
-
+      <div className="h-[35%] w-full flex justify-center items-center">
+        <textarea
+          className={
+            !textArea
+              ? "w-0 smooth py-6"
+              : "w-2/3 py-6 px-10 rounded-full smooth shadow-2xl"
+          }
+          readOnly
+          value={textVal}
+        ></textarea>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
